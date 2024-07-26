f49c8c6596b4e1b7e07eb55f1d0ac20807b560cd
Brian Watson <brian_watson@esri.com>
2024-07-26 09:33:38 -0400

    Day 2

---
+++ 4-constants/constant.go
@@ -0,0 +1,30 @@
+// Go supports constans of:
+// character
+// string
+// boolean
+// numeric values
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	// Constant expressions perform arithmetic with arbitrary precision
+	const d = 3e20 / n
+
+	// Numeric constant has no type until it's given one
+	fmt.Println(int64(d))
+
+	// Number can be given a type by using it a context that requires one, such as variable assignment or function call
+	// Here math.Sin expects a float64
+	fmt.Println(math.Sin(n))
+}
---
+++ 5-for/for.go
@@ -0,0 +1,39 @@
+// For is Go's only looping construct
+package main
+
+import "fmt"
+
+func main() {
+
+	// Most basic, one condition
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// classic initial/condition,after for loop
+	for j := 0; j < 3; j++ {
+		fmt.Println(j)
+	}
+
+	// loop over a range N times
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
+
+	// for without a condition-- will look until a `break` or `return`
+	for {
+		fmt.Println("no condition loop")
+		break
+	}
+
+	// also can `continue` to next iteration of loop
+	// here we should only print odds
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
