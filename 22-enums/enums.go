0b1fbb6cb65c806f30136a8eb028b5e9fb1205c8
Brian Watson <brian_watson@esri.com>
2024-08-04 23:58:47 -0400

    feat: day 11, enums WIP

--- 22-enums/enums.go
+++ 22-enums/enums.go
@@ -2,6 +2,8 @@
 // Go doesn't have an enum type as a language feature, but enums are implemented using existing language idioms
 package main
 
+import "fmt"
+
 // Our enum type ServerState has underlying int type
 type ServerState int
 
@@ -13,6 +15,8 @@ const (
 	StateRetrying
 )
 
+// Map with possible string values.  Can be cumbersome with many.  string tool can be used:  https://pkg.go.dev/golang.org/x/tools/cmd/stringer
+// Also relevant:  https://eli.thegreenplace.net/2021/a-comprehensive-guide-to-go-generate
 var stateName = map[ServerState]string{
 	StateIdle:      "idle",
 	StateConnected: "connected",
@@ -23,4 +27,19 @@ var stateName = map[ServerState]string{
 // Implementing fmt.Stringer interface.  https://pkg.go.dev/fmt#Stringer
 func (s ServerState) String() string {
 	return stateName[s]
+}
+
+// Emulates a state transition for a server
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+	return StateConnected
 }
