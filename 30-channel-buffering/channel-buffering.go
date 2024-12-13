b7f610dc92f89f3a36ccbbd7dd27bd2faab20624
Brian Watson <brian_watson@esri.com>
2024-12-13 09:35:43 -0500

    chore: cleaning up comment

--- 30-channel-buffering/channel-buffering.go
+++ 30-channel-buffering/channel-buffering.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// By default, channels are unbuffered, meaning that they will only accept sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value. Buffered channels accept a limited number of values without a corresponding receiver for those values.
+	// By default, channels are unbuffered, meaning that they will only accept sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value.
+	// Buffered channels accept a limited number of values without a corresponding receiver for those values.
 	messages := make(chan string, 2)
 
 	// Because this channel is buffered, we can send these values into the channel without a corresponding concurrent receive
