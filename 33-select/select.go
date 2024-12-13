672eb0e99c2f20e2a138ff13ff36458ac83699c5
Brian Watson <brian_watson@esri.com>
2024-12-13 10:07:37 -0500

    feat: adding select

---
+++ 33-select/select.go
@@ -0,0 +1,38 @@
+// Select is a Go statement that lets a goroutine wait on multiple communication operations.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("\nExecution time: ", elapsed)
+
+}
