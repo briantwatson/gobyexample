25b64666e78aaca89957e264ab1dc2d7326f8317
Brian Watson <brian_watson@esri.com>
2024-12-16 10:28:19 -0500

    docs: adding comments

--- 33-select/select.go
+++ 33-select/select.go
@@ -13,6 +13,7 @@ func main() {
 	c1 := make(chan string)
 	c2 := make(chan string)
 
+	// Each channel will receive a value after some amount of time, to simulate e.g. blocking RPC operations executing in concurrent goroutines.
 	go func() {
 		time.Sleep(1 * time.Second)
 		c1 <- "one"
@@ -24,6 +25,7 @@ func main() {
 	}()
 
 	for i := 0; i < 2; i++ {
+		// We use a select to await both these values simultaneously, printing each one as it arrives.
 		select {
 		case msg1 := <-c1:
 			fmt.Println("received", msg1)
@@ -32,6 +34,7 @@ func main() {
 		}
 	}
 
+	// Total execution time is ~2 seconds since both sleeps execute concurrently.
 	elapsed := time.Since(start)
 	fmt.Println("\nExecution time: ", elapsed)
 
