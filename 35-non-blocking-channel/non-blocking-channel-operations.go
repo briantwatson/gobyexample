207ef97f9ed069eebc7863a8eff6d37470814252
Brian Watson <brian_watson@esri.com>
2024-12-17 09:46:41 -0500

    chore: further experimentation on channel operations

--- 35-non-blocking-channel/non-blocking-channel-operations.go
+++ 35-non-blocking-channel/non-blocking-channel-operations.go
@@ -33,4 +33,46 @@ func main() {
 	default:
 		fmt.Println("no activity")
 	}
+
+	// buffered channels
+	messages2 := make(chan string, 1)
+	signals2 := make(chan bool, 1)
+
+	msg2 := "hi"
+	signals2 <- true
+
+	select {
+	case messages2 <- msg2:
+		fmt.Println("sent message", msg2)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	// Here because both channels have a message, go randomly selects one of the two cases to execute.
+	select {
+	case msg := <-messages2:
+		fmt.Println("received message", msg)
+	case sig := <-signals2:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+
+	// Handling multiple channels with a for loop
+	messages3 := make(chan string, 1)
+	signals3 := make(chan bool, 1)
+
+	messages3 <- "hi"
+	signals3 <- true
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg := <-messages3:
+			fmt.Println("received message", msg)
+		case sig := <-signals3:
+			fmt.Println("received signal", sig)
+		default:
+			fmt.Println("no activity")
+		}
+	}
 }
